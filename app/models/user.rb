76d76f950cec3b05953ba9bcaba01238b7ed939b
Aleksey Rykov <a.rykov8923@gmail.com>
2018-12-10 11:48:10 +1100

    Fix
    fix mistake in last commit, stage necessary file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable, :validatable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+
+  validates :username, format: { with: /\A[a-zA-Z][a-zA-Z0-9]+\z/,
+                                    message: "only allows letters and integers" }
+  validates :firstname, :lastname, format: { with: /\A[а-яА-ЯёЁ\s]+\z/,
+                                 message: "Допустимы только русские буквы" }, allow_blank: true
+  validates :password, length: { minimum: 5 }
+
+  validates :email, format: { with: Devise.email_regexp, message: "invalid email" }
 end
